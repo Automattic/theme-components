457f56558d2ca41d81a0a1322704467764d1ced4
Tammie Lister <karmatosed@gmail.com>
2015-03-17 18:30:30 +0000

    Templates

---
+++ comments.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying comments.
+ *
+ * The area of the page that contains both current comments
+ * and the comment form.
+ *
+ * @package atomic_s
+ */
+
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+
+//elements/structures/comments/comments.php
---
+++ header.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package atomic_s
+ */
+
+//elements/structures/global/header.php
