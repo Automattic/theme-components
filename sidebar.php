3850fd48488df3b7780d81f94d39fd45caba87f5
Tammie Lister <tammie@automattic.com>
2015-06-10 15:18:23 +0100

    Add in social menus

--- sidebar.php
+++ sidebar.php
@@ -11,5 +11,8 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <div id="secondary" class="widget-area" role="complementary">
+
+
+
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->
