2455beb1cdf1dcd69b2ba02e3ad9a47b9159df99
Tammie Lister <tammie@automattic.com>
2015-07-10 00:03:55 +0100

    Files for blog type

---
+++ types/blog-traditional/sidebar.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The sidebar containing the main widget area.
+ *
+ * @package theme-traditional
+ */
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
+
+<div id="secondary" class="widget-area" role="complementary">
+
+	<! additional -->
+
+	<?php if ( has_nav_menu ( 'social' ) ) : ?>
+		<?php wp_nav_menu( array(
+			'theme_location' => 'social',
+			'depth' => 1,
+			'link_before' => '<span class="screen-reader-text">',
+			'link_after' => '</span>',
+			'container_class' => 'social-links',
+		) ); ?>
+	<?php endif; ?>
+
+	<! additional -->
+
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</div><!-- #secondary -->
