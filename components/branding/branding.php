06ff4c9a316ceb4b66dc353f453a61c47ac61d45
sarah <sarah@triggersandsparks.com>
2015-11-22 02:02:42 +0000

    Mirror heading structure used in _s.

--- components/branding/branding.php
+++ components/branding/branding.php
@@ -1,4 +1,8 @@
 <div class="site-branding">
-	<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-	<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+	<?php if ( is_front_page() && is_home() ) : ?>
+		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+	<?php else : ?>
+		<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+	<?php endif; ?>
+	<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 </div><!-- .site-branding -->
