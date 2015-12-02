fe2f9bf68827f2c4ee7cd2e2d3067f542e5c2663
laurelfulford <laurelfulford@gmail.com>
2015-12-02 09:48:08 -0800

    Fixing double underscore in component_s_site_logo_init function reference

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -39,7 +39,7 @@ function component_s_site_logo_init() {
 	add_image_size( 'component-s-logo', 200, 200 );
 	add_theme_support( 'site-logo', array( 'size' => 'component-s-logo' ) );
 }
-add_action( 'after_setup_theme', 'component_s__site_logo_init' );
+add_action( 'after_setup_theme', 'component_s_site_logo_init' );
 
 /**
  * Return early if Site Logo is not available.
