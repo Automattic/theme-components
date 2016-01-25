f80c952cebde2d40e05f95ddc53aae2d4db282f7
David A. Kennedy <me@davidakennedy.com>
2016-01-25 16:23:50 -0500

    Fix issue where posts do not load on index page

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ get_header(); ?>
 						 * If you want to override this in a child theme, then include a file
 						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 						 */
-						get_template_part( 'content', get_post_format() );
+						get_template_part( 'components/content/content', get_post_format() );
 					?>
 
 				<?php endwhile; ?>
