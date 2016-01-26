6741425c1417514ffc50e64f0b37a033bb2ed55d
David A. Kennedy <me@davidakennedy.com>
2016-01-26 13:22:36 -0500

    Fix broken call to file in `single-jetpack-portfolio.php`

--- single-jetpack-portfolio.php
+++ single-jetpack-portfolio.php
@@ -12,7 +12,7 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'content', 'portfolio-single' ); ?>
+			<?php get_template_part( 'components/content', 'portfolio-single' ); ?>
 
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
