9f410678fa3f9f3d6767456cf67a88b51ea48420
David A. Kennedy <me@davidakennedy.com>
2016-02-08 14:13:46 -0500

    Capitalize "C" in places and change text to work with generator

--- functions.php
+++ functions.php
@@ -17,7 +17,7 @@ function components_setup() {
 	/*
 	 * Make theme available for translation.
 	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on components, use a find and replace
+	 * If you're building a theme based on Components, use a find and replace
 	 * to change 'components' to the name of your theme in all the template files
 	 */
 	load_theme_textdomain( 'components', get_template_directory() . '/languages' );
