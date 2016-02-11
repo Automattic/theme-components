9cd483717d402af81f2cd1bf33d77e05a79cab52
rdswd <developer@rdswebdesign.com>
2016-02-11 15:56:02 -0600

    Issue #201 fixing template part slug
    the get_template_part slug doesn't exist and is missing the subdirector - content-hero and testimonials. See issue #201 https://github.com/Automattic/theme-components/issues/201

--- page-templates/template-front.php
+++ page-templates/template-front.php
@@ -9,8 +9,8 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<?php get_template_part( 'components/content', 'hero' ); ?>
-			<?php get_template_part( 'components/testimonials' ); ?>
+			<?php get_template_part( 'components/content-hero/content', 'hero' ); ?>
+			<?php get_template_part( 'components/testimonials/testimonials' ); ?>
 		</main>
 	</div>
 
