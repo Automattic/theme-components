cdb376f5d271b49c43b12b921ea478f8b91fa1f6
David A. Kennedy <me@davidakennedy.com>
2016-03-16 12:36:08 -0400

    Fix typos in function names.

--- sidebar.php
+++ sidebar.php
@@ -7,11 +7,11 @@
  * @package Components
  */
 
-if ( ! is_activecomponentsidebar( 'sidebar-1' ) ) {
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
 
 <aside id="secondary" class="widget-area" role="complementary">
-	<?php dynamiccomponentsidebar( 'sidebar-1' ); ?>
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
