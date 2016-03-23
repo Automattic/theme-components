f6885d64104bacb254697f466099fac971129de6
laurelfulford <laurelfulford@gmail.com>
2016-03-23 11:53:23 -0700

    Updating paths to get_template_parts for index

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ get_header(); ?>
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-				get_template_part( 'components/content', get_post_format() );
+				get_template_part( 'components/post/content', get_post_format() );
 
 			endwhile;
 
@@ -42,7 +42,7 @@ get_header(); ?>
 
 		else :
 
-			get_template_part( 'components/content', 'none' );
+			get_template_part( 'components/post/content', 'none' );
 
 		endif; ?>
 
