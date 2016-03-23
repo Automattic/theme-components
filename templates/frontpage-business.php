7e97c36baef38a0ba084bfe48b4bb4118597c93f
laurelfulford <laurelfulford@gmail.com>
2016-03-23 12:42:26 -0700

    Wrapping hero-image template part in while loop in frontpage-business.

--- templates/frontpage-business.php
+++ templates/frontpage-business.php
@@ -9,10 +9,11 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-	
-			<?php get_template_part( 'components/features/hero-image/content', 'hero' ); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'components/features/hero-image/content', 'hero' ); ?>
+			<?php endwhile; ?>
+
 			<?php get_template_part( 'components/features/testimonials/content', 'testimonials' ); ?>
-	
 		</main>
 	</div>
 
