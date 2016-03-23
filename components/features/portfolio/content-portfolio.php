31508a17ac74d4aeaee8a33960e3c848769987da
laurelfulford <laurelfulford@gmail.com>
2016-03-23 15:25:05 -0700

    Adding escaping to translated string.

--- components/features/portfolio/content-portfolio.php
+++ components/features/portfolio/content-portfolio.php
@@ -18,6 +18,6 @@
 	<header class="portfolio-entry-header">
 		<?php the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' ); ?>
 
-		<?php echo get_the_term_list( get_the_ID(), 'jetpack-portfolio-type', '<span class="portfolio-entry-meta">', _x(', ', 'Used between list items, there is a space after the comma.', 'components' ), '</span>' ); ?>
+		<?php echo get_the_term_list( get_the_ID(), 'jetpack-portfolio-type', '<span class="portfolio-entry-meta">', esc_html_x(', ', 'Used between list items, there is a space after the comma.', 'components' ), '</span>' ); ?>
 	</header><!-- .portfolio-entry-header -->
 </article><!-- #post-## -->
