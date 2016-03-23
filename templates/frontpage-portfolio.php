f642dd5adebdc7c7f9f6be5a184dbfc320577c26
laurelfulford <laurelfulford@gmail.com>
2016-03-23 15:10:36 -0700

    Adding loop to portfolio frontpage.

--- templates/frontpage-portfolio.php
+++ templates/frontpage-portfolio.php
@@ -9,10 +9,13 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		
-			<?php get_template_part( 'components/page/content', 'page' ); ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'components/page/content', 'page' ); ?>
+			<?php endwhile; ?>
+
 			<?php get_template_part( 'components/feature/portfolio/portfolio' ); ?>
-		
+
 		</main>
 	</div>
 
