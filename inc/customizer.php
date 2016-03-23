3d1f2f5a023b9f8ca97b05d8019a945daa05e08a
laurelfulford <laurelfulford@gmail.com>
2016-03-23 11:49:25 -0700

    Replacing getcomponentsetting with get_setting; replacing wp_enqueuecomponentscript with wp_enqueue_script. Making components uppercase in comments.

--- inc/customizer.php
+++ inc/customizer.php
@@ -11,9 +11,9 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function components_customize_register( $wp_customize ) {
-	$wp_customize->getcomponentsetting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->getcomponentsetting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->getcomponentsetting( 'header_textcolor' )->transport = 'postMessage';
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 }
 add_action( 'customize_register', 'components_customize_register' );
 
@@ -21,6 +21,6 @@ add_action( 'customize_register', 'components_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function components_customize_preview_js() {
-	wp_enqueuecomponentscript( 'components_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'components_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'components_customize_preview_js' );
