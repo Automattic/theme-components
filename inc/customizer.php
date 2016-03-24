bc9393ba742b60e98317138f3a9510a3db1c6e13
Caroline Moore <calobee@gmail.com>
2016-03-24 12:14:49 -0400

    Point customizer.php to the correct JS path

--- inc/customizer.php
+++ inc/customizer.php
@@ -21,6 +21,6 @@ add_action( 'customize_register', 'components_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function components_customize_preview_js() {
-	wp_enqueue_script( 'components_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'components_customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'components_customize_preview_js' );
