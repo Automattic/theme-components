5d02bd2c589b206357e0c316d277d64da5400118
Caroline Moore <calobee@gmail.com>
2016-03-24 12:51:57 -0400

    Fix find and replace error in docblock

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ if ( ! function_exists( 'components_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
  *
- * Note that this function is hooked into the aftercomponentsetup_theme hook, which
+ * Note that this function is hooked into the after_setup_theme hook, which
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
