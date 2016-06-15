5492d222513fbe007d2ffdc36dca990700a3d306
Caroline Moore <calobee@gmail.com>
2016-06-15 16:53:09 -0400

    Remove post formats from functions.php

--- functions.php
+++ functions.php
@@ -61,18 +61,6 @@ function components_setup() {
 		'caption',
 	) );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See https://developer.wordpress.org/themes/functionality/post-formats/
-	 */
-	add_theme_support( 'post-formats', array(
-		'aside',
-		'image',
-		'video',
-		'quote',
-		'link',
-	) );
-
 	// Set up the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'components_custom_background_args', array(
 		'default-color' => 'ffffff',
