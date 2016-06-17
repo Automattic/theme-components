53cb43f303171f2422e18a9e04a353a2a1d667f8
Caroline Moore <calobee@gmail.com>
2016-06-17 10:55:03 -0400

    Adds three-column layout functionality to Magazine type.

---
+++ types/magazine/sidebar-2.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The sidebar containing the second widget area.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Components
+ */
+
+if ( ! is_active_sidebar( 'sidebar-2' ) ) {
+	return;
+}
+?>
+
+<aside id="tertiary" class="widget-area" role="complementary">
+	<?php dynamic_sidebar( 'sidebar-2' ); ?>
+</aside><!-- #tertiary -->
