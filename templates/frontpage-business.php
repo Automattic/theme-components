015c4a7c7dceed182b1961b217c059a94cecc253
Bappi <bappi.cs@gmail.com>
2016-08-09 04:28:11 +0600

    Add end comments

--- templates/frontpage-business.php
+++ templates/frontpage-business.php
@@ -14,7 +14,7 @@ get_header(); ?>
 			<?php endwhile; ?>
 
 			<?php get_template_part( 'components/features/testimonials/testimonials' ); ?>
-		</main>
-	</div>
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
 
 <?php get_footer(); ?>
